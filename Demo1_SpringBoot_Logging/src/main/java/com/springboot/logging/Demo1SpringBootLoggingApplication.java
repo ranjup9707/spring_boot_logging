f8935295550deafbca8636493de53c0e6c49f1e4
Ranju <ranjupillai2016@gmail.com>
2023-07-11 18:53:52 +0530

    Initial Commit

---
+++ Demo1_SpringBoot_Logging/src/main/java/com/springboot/logging/Demo1SpringBootLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1SpringBootLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1SpringBootLoggingApplication.class, args);
+	}
+
+}
---
+++ Demo1_SpringBoot_Logging/src/test/java/com/springboot/logging/Demo1SpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1SpringBootLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
